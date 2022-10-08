496c06499d39c41a6a4c95154b39d0ce717957c4
Ioannis Chantolios <ioannis.chantolios@ieee.org>
2022-10-08 11:15:52 +0100

    debug line 7

--- leaptest.sh
+++ leaptest.sh
@@ -4,7 +4,8 @@
 echo "Type the year (4 numbers), followed by the return key"
 
 read year
-if (( ("$year" % 400) == "0")) || (( ("%year" % 4 == "0") && ("$year" % 100 != "0") ));then
+if (( ("$year" % 400) == "0" )) || (( ("$year" % 4 == "0") && ("$year" % 100 !=
+"0") ));then
 	echo "$year is a leap year."
 else
 	echo "This is not a leap year"
